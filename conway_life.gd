6e208c17ab78fc8679996c2fde2ad9da025b21f2
Jackson Swenson <Jackson.Swenson@walmart.com>
2019-07-05 05:06:22 -0500

    crashes after a little time

--- conway_life.gd
+++ conway_life.gd
@@ -3,8 +3,13 @@ extends Spatial
 #from random import randint, seed
 #from time import time
 
-var grid_size = [ 50, 50 ]
+# grid_size / height
+# 50 / 100
+# 20 / 15
+var grid_size = [ 20, 20 ]
 var tick_rate = 100
+var camera_height = 15
+var camera_step = 1
 
 #screen = pygame.display.set_mode(size)
 #pygame.display.set_caption("Conway's Game of Life")
@@ -26,39 +31,10 @@ onready var camera = get_node('camera')
 var cur = 0
 var nxt = 1
 var z = 0
-var camera_height = 50
+
+var paused = false
 
 var grid_seed = 100
-
-func tick(x, y, z):
-    var life = 0
-
-    for a in range(max(x - 1, 0), min(x + 2, grid_size[0])):
-        for b in range(max(y - 1, 0), min(y + 2, grid_size[1])):
-            if a == x and b == y:
-                continue
-            if grid[a][b][cur] == ALIVE:
-                life += 1
-
-    if life < 2 or life > 3:
-        grid[x][y][nxt] = DEAD
-
-    elif grid[x][y][cur] == ALIVE and (life == 2 or life == 3):
-        grid[x][y][nxt] = ALIVE
-
-    elif grid[x][y][cur] == DEAD and life == 3:
-        grid[x][y][nxt] = ALIVE
-
-    else:
-        grid[x][y][nxt] = grid[x][y][cur]
-
-    if grid[x][y][nxt] == ALIVE:
-        var new_unit = unit.instance()
-        #new_unit.transform.translated(Vector3(x, y, z))
-        new_unit.translation = Vector3(x, z, y)
-        add_child(new_unit)
-        #new_unit.show()
-        print('alive! ', new_unit.translation)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -71,20 +47,55 @@ func _ready():
         for y in range(grid_size[1]):
             grid[x].append([ randi() % (ALIVE + 1), randi() % (ALIVE + 1) ])
 
+func _fixed_process(delta):
+    
+    if Input.is_key_pressed(SPKEY):
+        print('space')
+
 onready var time_sum = 0.0
 
 func _process(delta):
     time_sum += delta
     
-    if time_sum > 60.0 / tick_rate:
+    if not paused and time_sum > 60.0 / tick_rate:
         time_sum = 0.0
+        
+        # tick all cells
         for x in range(grid_size[0]):
             for y in range(grid_size[1]):
-                tick(x, y, z)
+                
+                var life = 0
+                
+                # find surrounding live cells
+                for a in range(max(x - 1, 0), min(x + 2, grid_size[0])):
+                    for b in range(max(y - 1, 0), min(y + 2, grid_size[1])):
+                        if a == x and b == y:
+                            continue
+                        if grid[a][b][cur] == ALIVE:
+                            life += 1
+            
+                # set next state
+                if life < 2 or life > 3:
+                    grid[x][z][nxt] = DEAD
+            
+                elif grid[x][z][cur] == ALIVE and (life == 2 or life == 3):
+                    grid[x][z][nxt] = ALIVE
+            
+                elif grid[x][z][cur] == DEAD and life == 3:
+                    grid[x][z][nxt] = ALIVE
+            
+                else:
+                    grid[x][z][nxt] = grid[x][z][cur]
+                
+                # instance node
+                if grid[x][z][nxt] == ALIVE:
+                    var new_unit = unit.instance()
+                    new_unit.translation = Vector3(x, z, y)
+                    add_child(new_unit)
                 
         z += 1
         var tmp = cur
         cur = nxt
         nxt = tmp
         
-        #camera.translate(Vector3(0, 0, 5))
+        camera.translate(Vector3(0, 0, camera_step))
