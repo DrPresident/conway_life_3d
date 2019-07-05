42e7455256bef5ae536575e4a00858e2da3a87e4
Jackson Swenson <Jackson.Swenson@walmart.com>
2019-07-05 00:50:55 -0500

    kind of working

---
+++ conway_life.gd
@@ -0,0 +1,90 @@
+extends Spatial
+
+#from random import randint, seed
+#from time import time
+
+var grid_size = [ 50, 50 ]
+var tick_rate = 100
+
+#screen = pygame.display.set_mode(size)
+#pygame.display.set_caption("Conway's Game of Life")
+
+var DEAD  = 0
+var ALIVE = 1
+
+var COLORS = [
+    [0, 0, 0],
+    [0, 150, 0]
+]
+
+var grid = []
+
+onready var unit = preload('res://unit.tscn')
+onready var camera = get_node('camera')
+
+# will determine the cur and nxt
+var cur = 0
+var nxt = 1
+var z = 0
+var camera_height = 50
+
+var grid_seed = 100
+
+func tick(x, y, z):
+    var life = 0
+
+    for a in range(max(x - 1, 0), min(x + 2, grid_size[0])):
+        for b in range(max(y - 1, 0), min(y + 2, grid_size[1])):
+            if a == x and b == y:
+                continue
+            if grid[a][b][cur] == ALIVE:
+                life += 1
+
+    if life < 2 or life > 3:
+        grid[x][y][nxt] = DEAD
+
+    elif grid[x][y][cur] == ALIVE and (life == 2 or life == 3):
+        grid[x][y][nxt] = ALIVE
+
+    elif grid[x][y][cur] == DEAD and life == 3:
+        grid[x][y][nxt] = ALIVE
+
+    else:
+        grid[x][y][nxt] = grid[x][y][cur]
+
+    if grid[x][y][nxt] == ALIVE:
+        var new_unit = unit.instance()
+        #new_unit.transform.translated(Vector3(x, y, z))
+        new_unit.translation = Vector3(x, z, y)
+        add_child(new_unit)
+        #new_unit.show()
+        print('alive! ', new_unit.translation)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    camera.translation = Vector3(grid_size[0] / 2, camera_height, grid_size[0] / 2)
+    
+    seed(grid_seed)
+    # initialize grid
+    for x in range(grid_size[0]):
+        grid.append([])
+        for y in range(grid_size[1]):
+            grid[x].append([ randi() % (ALIVE + 1), randi() % (ALIVE + 1) ])
+
+onready var time_sum = 0.0
+
+func _process(delta):
+    time_sum += delta
+    
+    if time_sum > 60.0 / tick_rate:
+        time_sum = 0.0
+        for x in range(grid_size[0]):
+            for y in range(grid_size[1]):
+                tick(x, y, z)
+                
+        z += 1
+        var tmp = cur
+        cur = nxt
+        nxt = tmp
+        
+        #camera.translate(Vector3(0, 0, 5))
